471a16c865a55112937501493d96f2901c0f4f9f
Decembboy <debor9130@gmail.com>
2024-07-30 20:11:20 +0530

    Add files via upload

---
+++ change only first letter.py
@@ -0,0 +1,15 @@
+word="dog"
+enter=input(" ")
+if len(enter)==1:
+    rp=word.replace(word[0],enter)
+    print(rp)
+    
+else:
+    print("contain only a character")
+    
+    
+    
+   
+    
+    
+    
